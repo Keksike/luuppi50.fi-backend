a835578bdc90a067a71f6272e6c656d6839943e6
Cihan Bebek <cihan.m.bebek@gmail.com>
2018-11-07 18:48:36 +0000

    add user email stuff, dont take into use yet

--- handler.js
+++ handler.js
@@ -28,7 +28,7 @@ function generateError(code, err) {
   };
 }
 
-function generateEmailParams(body) {
+function generateReportEmailParams(body) {
   const { content } = JSON.parse(body);
 
   if (!content) {
@@ -42,6 +42,7 @@ name: ${content.name}
 email: ${content.email}
 phone: ${content.phone}
 organization: ${content.organization}
+startingYear: ${content.startingYear}
 cocktail: ${content.cocktail}
 greeting: ${content.greeting}
 drink: ${content.drink}
@@ -53,18 +54,11 @@ isAvecOfInvitee: ${content.isAvecOfInvitee}
 other: ${content.other}
 `;
 
-  console.log('sending email:' + emailContent);
-
   return {
     Source: myEmail,
     Destination: { ToAddresses: [myEmail] },
     Message: {
-      Body: {
-        Text: {
-          Charset: 'UTF-8',
-          Data: emailContent,
-        },
-      },
+      Body: { Text: { Charset: 'UTF-8', Data: emailContent } },
       Subject: {
         Charset: 'UTF-8',
         Data: `LUUPPI50: Ilmoittautuminen henkilöltä: ${content.name}`,
@@ -73,10 +67,56 @@ other: ${content.other}
   };
 }
 
+function generateUserEmailParams(body) {
+  const { content } = JSON.parse(body);
+
+  if (!content) {
+    throw new Error(
+      "Missing parameters! Make sure to add parameters 'content'.",
+    );
+  }
+
+  const emailContent = `
+Hei!
+
+Kiitos ilmoittautumisestasi Luuppi ry:n vuosijuhliin. Vuosijuhlat voit maksaa Luuppi ry:n tilille FI68 8216 9710 0006 17 ja laittamalla kommenttikenttään viestin muotoa ”oma nimi/Luuppi50”. Mikäli maksat myös avecin samalla maksulla, ilmoitathan myös tästä kommenttikentässä. Maksu tulee suorittaa ilmoittautumisen jälkeen, viimeistään kuitenkin 25.11.
+
+Lisätietoa vuosijuhlista saat lähempänä itse juhlia sekä tietenkin vuosijuhlien verkkosivuilta https://luuppi50.fi. Juhlissa nähdään!
+
+Juhlavin terveisin,
+Vuosijuhlatiimi
+vuosijuhlat@luuppi.fi
+`;
+
+  console.log('sending email to user:' + emailContent);
+
+  return {
+    Source: myEmail,
+    Destination: { ToAddresses: [content.email] },
+    Message: {
+      Body: { Text: { Charset: 'UTF-8', Data: emailContent } },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `Luupin 50-vuotisjuhlien ilmoittautumisen vahvistus`,
+      },
+    },
+  };
+}
+
 module.exports.send = async event => {
   try {
-    const emailParams = generateEmailParams(event.body);
-    const data = await ses.sendEmail(emailParams).promise();
+    const reportEmailParams = generateReportEmailParams(event.body);
+    console.log(
+      'sending email to reporting:' + JSON.stringify(reportEmailParams),
+    );
+    const data = await ses.sendEmail(reportEmailParams).promise();
+
+    // const userEmailParams = generateUserEmailParams(event.body);
+    // console.log(
+    //   'sending email to reporting:' + JSON.stringify(userEmailParams),
+    // );
+    // const emailMailData = await ses.sendEmail(userEmailParams).promise();
+
     return generateResponse(200, data);
   } catch (err) {
     return generateError(500, err);
